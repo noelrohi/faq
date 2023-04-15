c3b2faa7c34be535f0eaf0bc176b7f823829330d
gneiru <noelrohi59@gmail.com>
2023-04-15 14:07:16 +0800

    feat(nav) : github link

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,3 +1,4 @@
+import { GithubIcon } from "lucide-react";
 import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import { Avatar, AvatarFallback, AvatarImage } from "~/ui/avatar";
@@ -16,7 +17,7 @@ export const NavBar = () => {
 
   return (
     <>
-      <header className="sticky top-0 z-40 w-full border-b border-b-slate-200 bg-white px-12 dark:border-b-slate-700 dark:bg-slate-900">
+      <header className="sticky top-0 z-40 w-full border-b border-b-slate-200 bg-white px-4 lg:pl-16 dark:border-b-slate-700 dark:bg-slate-900">
         <div className="container flex h-16 items-center">
           <div className="flex gap-4">
             <Link className="flex items-center gap-4 space-x-2" href="/faq">
@@ -26,8 +27,13 @@ export const NavBar = () => {
             </Link>
           </div>
 
-          <div className="flex flex-1 items-center space-x-2 sm:space-x-4 justify-end">
-            <div className="flex flex-row items-center justify-center gap-4">
+          <div className="flex flex-1 items-center justify-end">
+            <div className="flex flex-row items-center justify-center gap-8 lg:gap-14">
+              <div>
+                <Link href="https://github.com/gneiru/faq">
+                  <GithubIcon />
+                </Link>
+              </div>
               <div>
                 {!sessionData && (
                   <Button variant="outline" onClick={() => void signIn()}>
@@ -36,13 +42,11 @@ export const NavBar = () => {
                 )}
 
                 {sessionData && (
-                  <>
-                    <UserDropdown
-                      src={sessionData.user.image ?? ""}
-                      alt={sessionData.user.name ?? ""}
-                      fallback={sessionData.user.name?.slice(0, 1) ?? "SH"}
-                    />
-                  </>
+                  <UserDropdown
+                    src={sessionData.user.image ?? ""}
+                    alt={sessionData.user.name ?? ""}
+                    fallback={sessionData.user.name?.slice(0, 1) ?? "SH"}
+                  />
 
                   //   <Button variant="outline" onClick={() => void signIn()}>
                   //     Sign In
