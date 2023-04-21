252be19a22e807131b0418eaf0a1bb78a5f3669e
gneiru <noelrohi59@gmail.com>
2023-04-21 16:11:50 +0800

    add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { Analytics } from "@vercel/analytics/react";
 
 import { api } from "~/utils/api";
 
@@ -17,6 +18,7 @@ const MyApp: AppType<{ session: Session | null }> = ({
       <NavBar />
       <Toaster />
       <Component {...pageProps} />
+      <Analytics />
     </SessionProvider>
   );
 };
