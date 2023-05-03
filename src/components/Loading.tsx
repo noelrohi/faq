c5285a512b9307beb58a8713b1e1a12c52b4cd0b
gneiru <noelrohi59@gmail.com>
2023-05-03 08:45:40 +0800

    fix light mode bg of skeleton load

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -12,13 +12,13 @@ export function LoadingSpinner(props: { size?: number }) {
 function CardSkeleton() {
   return (
     <div className="flex w-full flex-1 flex-row gap-4 rounded-md bg-white/10 p-4 shadow-md lg:w-1/3">
-      <Skeleton className="h-10 w-10 rounded-full bg-white dark:bg-slate-700" />
+      <Skeleton className="h-10 w-10 rounded-full bg-slate-300 dark:bg-slate-700" />
       <div className="flex">
         <div className="space-y-4">
-          <Skeleton className="h-6 w-[150] bg-white dark:bg-slate-700" />
-          <Skeleton className="h-4 w-[300px] bg-white dark:bg-slate-700" />
-          <Skeleton className="h-10 w-full bg-white dark:bg-slate-700" />
-          <Skeleton className="h-2 w-1/2 bg-white dark:bg-slate-700" />
+          <Skeleton className="h-6 w-[150] bg-slate-300 dark:bg-slate-700" />
+          <Skeleton className="h-4 w-[300px] bg-slate-300 dark:bg-slate-700" />
+          <Skeleton className="h-10 w-full bg-slate-300 dark:bg-slate-700" />
+          <Skeleton className="h-2 w-1/2 bg-slate-300 dark:bg-slate-700" />
         </div>
       </div>
     </div>
